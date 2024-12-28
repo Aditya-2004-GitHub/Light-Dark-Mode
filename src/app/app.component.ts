99cb8eae807643db2733c4bfd9f843d1133f52f1
Aditya-2004-GitHub <22f3001516@ds.study.iitm.ac.in>
2024-12-28 11:02:33 +0530

    theme

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,24 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'dark-light';
+
+  mode = 'lightMode'
+  toogle(){
+    if(this.mode == 'lightMode'){
+      this.mode = 'darkMode'
+    }
+    else{
+      this.mode = 'lightMode'
+    }
+  }
 }
